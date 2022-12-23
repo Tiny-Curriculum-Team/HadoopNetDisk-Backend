5321e4e3ea75768d46d776074c7bca2784c9ad6b
Breeze Shane <72027962+BreezeShane@users.noreply.github.com>
2022-12-23 20:40:59 +0800

    Uploaded docker-compose files of hadoop cluster.

---
+++ Hadoop_Cluster/namenode/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# /usr/local/hadoop/sbin/start-all.sh
+
+# /usr/local/hbase-2.2.2/bin/start-hbase.sh
+
+# usr/local/spark-2.4.0/sbin/start-all.sh
+
+# scp -r /usr/local/hbase-2.2.2 root@h02:/usr/local/
+# scp -r /usr/local/hbase-2.2.2 root@h03:/usr/local/
+# scp -r /usr/local/hbase-2.2.2 root@h04:/usr/local/
+# scp -r /usr/local/hbase-2.2.2 root@h05:/usr/local/
+echo " 启动成功"
+
+bash
