1d3b368ccd322186ae22c62fc6f477f3412f0a9a
Breeze Shane <72027962+BreezeShane@users.noreply.github.com>
2022-12-25 17:31:46 +0800

    Updated run shells.

--- start.cmd
+++ start.cmd
@@ -2,3 +2,6 @@ cd Hadoop_Cluster
 docker-compose up -d
 cd ..
 
+python HadoopNetdisk/manage.py makemigrations
+python HadoopNetdisk/manage.py migrate
+python HadoopNetdisk/manage.py runserver localhost:2731
