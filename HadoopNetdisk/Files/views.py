a63ab1c3adcc3b1defb19925b4241c5fa56106d7
RuoMengAwA <780337413@qq.com>
2022-12-31 01:20:55 +0800

    views update

--- HadoopNetdisk/Files/views.py
+++ HadoopNetdisk/Files/views.py
@@ -2,7 +2,19 @@ from utils import *
 
 
 def upload_files(request):
-    pass
+    # 接收到formdata的出文件之外的数据
+    data = request.POST
+    data
+
+    # 接收文件，getlist是接收多个文件
+    # formdata在vue中同一个key传入了多个value，value成为了一个数组，所以需要使用getlist来获取所有文件
+    new_files = request.FILES.getlist('new_files')
+
+    # formdata在vue中同一个key只有一个文件类型的value，可以使用get来获取文件
+    new_files = request.FILES.get('file')
+
+
+
 
 
 def download_files(request):
